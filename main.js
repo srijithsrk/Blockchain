192e9b505e0f8a62d29c0b31ad2419589eccf625
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-09-08 16:32:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,28 @@ class Blockchain {        s
 		this.createGenesisBlock()
 		];
 	}
+	createGenesisBlock(){    //its the first block containing no previous hash value
+		return new Block(0, "08/09/2018","Genesis Block","0");
+		                //index,timestamp,data,previous HashValue
+
+	}
+	getLatestBlock(){ //hash of this block is added to the next new block as previous blocks hash
+		return this.chain[this.chain.length-1];
+	}
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+
+		this.chain.push(newBlock);
+	}
+
+
+	let myCoin = new Blockchain(); //OOP'S application
+
+	myCoin.addBlock(new Block(1, "09/09/2018" , {amount:4}));
+	myCoin.addBlock(new Block(2, "10/09/2018" , {amount:8}));
+
+
+console.log(JSON.stringify(myCoin, null ,4));
+
+}
