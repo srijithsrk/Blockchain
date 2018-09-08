42f328d3925b5b30f4d3364cd65775897aadaf7f
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-09-09 02:04:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -44,6 +44,21 @@ class Blockchain
 
 		this.chain.push(newBlock);  //to push blocks into the blockchain
 	}
+	isChainValid(){
+		for(let i = 1; i < this.chain.length;i++){
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i-1];
+
+			if(currentBlock.hash !== currentBlock.calculateHash()){
+				return false;
+
+			}
+			if(currentBlock.previousHash !== previousBlock.hash){
+				return false;
+			}
+		}
+		return true;
+	}
 
 
 	let myCoin = new Blockchain(); //OOP'S application 
