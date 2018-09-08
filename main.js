93c796f52a0d6d1509dbf8209fcff474f8047692
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-09-08 16:43:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const SHA256 = require('crypto-js/sha256');  
-
+// its used for importing header files in the form of sha256
 class Block 
 {
 	constructor(index, timestamp, data, previousHash = '')
@@ -11,7 +11,7 @@ class Block
 		this,hash = this.calculateHash();
 	}
 
-	calculateHash()
+	calculateHash()  // this method calculates the hash value of the blocks
 	{         
 		return SHA256(this.index + this.timestamp +JSON.stringify(this.data) + this.previousHash).toString();  
 	}
@@ -33,7 +33,8 @@ class Blockchain
 
 	}
 	getLatestBlock()
-	{ //hash of this block is added to the next new block as previous blocks hash
+	{ 
+		//hash of this block is added to the next new block as previous blocks hash
 		return this.chain[this.chain.length-1];
 	}
 	addBlock(newBlock)
@@ -41,16 +42,17 @@ class Blockchain
 		newBlock.previousHash = this.getLatestBlock().hash;
 		newBlock.hash = newBlock.calculateHash();
 
-		this.chain.push(newBlock);
+		this.chain.push(newBlock);  //to push blocks into the blockchain
 	}
 
 
-	let myCoin = new Blockchain(); //OOP'S application
+	let myCoin = new Blockchain(); //OOP'S application 
 
-	myCoin.addBlock(new Block(1, "09/09/2018" , {amount:4}));
+	myCoin.addBlock(new Block(1, "09/09/2018" , {amount:4}));  //two blocks will be created with these parameters and will be pushed to blockchain 
 	myCoin.addBlock(new Block(2, "10/09/2018" , {amount:8}));
 
 
-console.log(JSON.stringify(myCoin, null ,4));
+console.log(JSON.stringify(myCoin, null ,4));   //displays output
 
 }
+
